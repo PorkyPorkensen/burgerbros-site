6e0a491d8914d2d91de1ee1fb24a6632c3a6541b
Mitz Bitz <mitchellvieira12@gmail.com>
2025-06-06 09:43:54 -0400

    css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return (
         <div className="foot">
-            <p>Burger Bros 2025 | My Github | My Other Projects</p>
+            <p>Burger Bros 2025 | <a className='footA' href="https://github.com/PorkyPorkensen">My Github</a> | <a className='footA' href="https://personal-site-ten-blush.vercel.app/">My Other Projects</a></p>
         </div>
     )
 }
